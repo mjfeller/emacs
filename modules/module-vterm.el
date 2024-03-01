317f5d89889696a41791ee7e855528e80213f96d
Mark Feller <mfeller@squareup.com>
2024-03-01 13:42:06 -0700

    vterm: fix parens

--- modules/module-vterm.el
+++ modules/module-vterm.el
@@ -23,7 +23,7 @@
 
 (use-package vterm
   :bind (("H-U" . vterm-other-window)
-         (:map vterm-mode-map ("M-P" . mjf/pash-copy))
+         (:map vterm-mode-map ("M-P" . mjf/pash-copy)))
   :config
   (add-to-list 'evil-emacs-state-modes 'vterm-mode)
   (add-to-list 'evil-emacs-state-modes 'compilation-mode))
