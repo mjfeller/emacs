cca18d2b013d4b31a9a674a859dd7c000fa92458
Mark Feller <mfeller@squareup.com>
2024-03-08 09:32:15 -0700

    notmuch: use consistent naming conventions

--- modules/module-notmuch.el
+++ modules/module-notmuch.el
@@ -26,7 +26,7 @@
 
 (require 'smtpmail)
 
-(defun mjf/initialize-gmail ()
+(defun mjf-initialize-gmail ()
   "Configure smtpmail to use gmail"
   (setq message-send-mail-function   'smtpmail-send-it
         send-mail-function           'smtpmail-send-it
@@ -36,7 +36,7 @@
         smtpmail-stream-type         'ssl
         smtpmail-smtp-service        465))
 
-(defun mjf/initialize-personal-email ()
+(defun mjf-initialize-personal-email ()
   "Configure smtpmail to use personal mail server"
   (setq message-send-mail-function   'smtpmail-send-it  ; message-mode
         send-mail-function           'smtpmail-send-it  ; mail-mode
@@ -46,34 +46,34 @@
         smtpmail-stream-type         'ssl
         smtpmail-smtp-service        465))
 
-(defun mjf/tag-deleted ()
+(defun mjf-tag-deleted ()
   "Tag mail at point as deleted. This operation will not delete
 the mail, but tag it for later deletion."
   (interactive)
   (notmuch-search-tag '("+deleted" "-unread"))
   (next-line))
 
-(defun mjf/tree-tag-deleted ()
+(defun mjf-tree-tag-deleted ()
   "Tag mail at point as deleted. This operation will not delete
 the mail, but tag it for later deletion."
   (interactive)
   (notmuch-tree-tag '("+deleted" "-unread"))
   (next-line))
 
-(defun mjf/delete-tagged-mail ()
+(defun mjf-delete-tagged-mail ()
   "Delete mail that is tagged with the deleted tag"
   (interactive)
   (shell-command "notmuch_delete")
   (notmuch-refresh-all-buffers))
 
-(defun mjf/getmail ()
+(defun mjf-getmail ()
   "Fetch mail using getmail"
   (interactive)
   (message "Fetching mail...")
   (shell-command "getmail --getmaildir=$HOME/.config/getmail")
   (notmuch-poll-and-refresh-this-buffer))
 
-(defun mjf/offlineimap ()
+(defun mjf-offlineimap ()
   "Fetch mail using getmail"
   (interactive)
   (message "Fetching mail...")
@@ -83,33 +83,26 @@ the mail, but tag it for later deletion."
 (use-package notmuch
   :bind
   (:map notmuch-search-mode-map
-        ("d" . mjf/tag-deleted)
-        ("D" . mjf/delete-tagged-mail)
+        ("d" . mjf-tag-deleted)
+        ("D" . mjf-delete-tagged-mail)
         ("/" . notmuch-search))
   (:map notmuch-tree-mode-map
-        ("d" . mjf/tree-tag-deleted)
-        ("D" . mjf/delete-tagged-mail))
+        ("d" . mjf-tree-tag-deleted)
+        ("D" . mjf-delete-tagged-mail))
   (:map notmuch-hello-mode-map
-        ("r" . mjf/offlineimap))
+        ("r" . mjf-offlineimap))
 
-  :config
-  (add-hook 'notmuch-hello-mode-hook 'disable-line-numbers)
-  (add-hook 'notmuch-message-mode-hook 'disable-line-numbers)
-  (add-hook 'notmuch-search-mode-hook 'disable-line-numbers)
-  (add-hook 'notmuch-show-mode-hook 'disable-line-numbers)
-  (add-hook 'notmuch-tree-mode-hook 'disable-line-numbers)
-
-  (setq notmuch-saved-searches
-        '((:name "inbox"        :query "tag:inbox"   :key "i")
-          (:name "unread"       :query "tag:unread"  :key "u")
-          (:name "all"          :query "*"           :key "a")
-          (:name "deleted"      :query "tag:deleted")
-          (:name "sent"         :query "tag:sent"    :key "t")
-          (:name "drafts"       :query "tag:draft"   :key "d")
-          (:name "work"         :query "mark@getsunday.com tag:unread" :key "w" :search-type tree)
-          (:name "OpenBSD tech" :query "tech@openbsd.org tag:unread" :search-type tree)
-          (:name "OpenBSD misc" :query "misc@openbsd.org tag:unread" :search-type tree)
-          )))
+  :custom
+  (notmuch-saved-searches
+   '((:name "inbox"        :query "tag:inbox"   :key "i")
+     (:name "unread"       :query "tag:unread"  :key "u")
+     (:name "all"          :query "*"           :key "a")
+     (:name "deleted"      :query "tag:deleted")
+     (:name "sent"         :query "tag:sent"    :key "t")
+     (:name "drafts"       :query "tag:draft"   :key "d")
+     (:name "work"         :query "mark@getsunday.com tag:unread" :key "w" :search-type tree)
+     (:name "OpenBSD tech" :query "tech@openbsd.org tag:unread" :search-type tree)
+     (:name "OpenBSD misc" :query "misc@openbsd.org tag:unread" :search-type tree))))
 
 (provide 'module-notmuch)
 
